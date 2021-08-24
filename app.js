bce743ff038347a216ad01a932e28d2fd268b6c1
joeyWuTRKTR <godatascial@yahoo.com>
2021-08-24 20:03:25 +0800

    feat: confirm user password and unique email

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+
+// 產生flash並放到session
+const flash = require('connect-flash')
+const session = require('express-session')
+
 const db = require('./models')
 const app = express()
 const port = 3000
@@ -8,7 +13,15 @@ const port = 3000
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(session({ secret:'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
 
 app.listen(port, () => {
   db.sequelize.sync()
--- controllers/userController.js
+++ controllers/userController.js
@@ -9,12 +9,28 @@ const userController = {
   },
   // 處理註冊行為
   signUp: (req, res) => {
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-    }).then(user => {
-      return res.redirect('/signin')
+    const { name, email, password, passwordCheck } = req.body
+    // 確認密碼和密碼不同
+    if ( password !== passwordCheck) {
+      req.flash('warning_msg', '密碼和密碼確認不相符!')
+      return res.redirect('/signup')
+    }
+
+    User.findOne({ where: {email: email} }).then(user => {
+      if (user) {
+        req.flash('warning_msg', '此信箱已註冊!')
+        return res.redirect('/signup')
+      } else {
+        User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+        }).then(user => {
+          // 註冊成功
+          req.flash('success_msg', '註冊成功!')
+          return res.redirect('/signin')
+        })
+      }
     })
   }
 }
