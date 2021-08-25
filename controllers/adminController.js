bd852db5df56de37a502bbaaf1726340c51c46cf
joeyWuTRKTR <godatascial@yahoo.com>
2021-08-25 16:04:33 +0800

    feat: add admin restaurants index page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,12 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+
 const adminController = {
   getRestaurants: (req, res) => {
     // 自動導到views文件夾，找到admin文件的restaurants.handlebars
-    return res.render('admin/restaurants')
+    Restaurant.findAll({ raw: true, nest: true }).then(restaurants => {
+      return res.render('admin/restaurants', restaurants)
+    })
   }
 }
 
