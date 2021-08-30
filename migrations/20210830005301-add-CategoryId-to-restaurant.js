c2b0570f0654bec55b8ff97d4bf5663a299edaf4
joeyWuTRKTR <godatascial@yahoo.com>
2021-08-30 09:21:24 +0800

    feat: add Category table and association with Restaurants

---
+++ migrations/20210830005301-add-CategoryId-to-restaurant.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const { query } = require("express");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Restaurants', 'CategoryId', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Categories',
+        key: 'id'
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Restaurants', 'CategoryId')
+  }
+};
